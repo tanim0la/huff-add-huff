af9b75e1a8a0a3ed64953fb5cce3beb27f63a9af
tanim0la <izukfred@gmail.com>
2023-02-10 16:29:21 +0100

    huff add huff

---
+++ test/AddNum.t.sol
@@ -0,0 +1,28 @@
+// SPDX-License-Identifier: Unlicense
+pragma solidity ^0.8.15;
+
+import "foundry-huff/HuffDeployer.sol";
+import "forge-std/Test.sol";
+import "forge-std/console.sol";
+
+contract AddNumTest is Test {
+    /// @dev Address of the AddNum contract.
+    AddNum public addNumm;
+
+    /// @dev Setup the testing environment.
+    function setUp() public {
+        addNumm = AddNum(HuffDeployer.deploy("AddNum"));
+    }
+
+    /// @dev Ensure that you can add two values and get the answer.
+    function testSetAndGetValue() public {
+        addNumm.addNum(3, 2);
+        assertEq(5, addNumm.getAnswer());
+    }
+}
+
+interface AddNum {
+    function addNum(uint256, uint256) external;
+
+    function getAnswer() external returns (uint256);
+}
